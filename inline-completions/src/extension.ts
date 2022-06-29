9a48d06b3032b2b5dccc83aedbafeda7fa6d3786
Jui Hanamshet (SHE/HER) <juihanamshet@microsoft.com>
2022-06-29 11:44:11 -0700

    bug repro

--- inline-completions/src/extension.ts
+++ inline-completions/src/extension.ts
@@ -11,42 +11,51 @@ export function activate(context: vscode.ExtensionContext) {
 	);
 
 	context.subscriptions.push(disposable);
-	let someTrackingIdCounter = 0;
 
-	const provider: vscode.InlineCompletionItemProvider = {
-		provideInlineCompletionItems: async (document, position, context, token) => {
-			console.log('provideInlineCompletionItems triggered');
-
-			const regexp = /\/\/ \[(.+),(.+)\):(.*)/;
-			if (position.line <= 0) {
-				return;
-			}
-
-			const lineBefore = document.lineAt(position.line - 1).text;
-			const matches = lineBefore.match(regexp);
-			if (matches) {
-				const start = matches[1];
-				const startInt = parseInt(start, 10);
-				const end = matches[2];
-				const endInt =
-					end === '*' ? document.lineAt(position.line).text.length : parseInt(end, 10);
-				const insertText = matches[3].replace(/\\n/g, '\n');
-
-				return [
-					{
-						insertText,
-						range: new vscode.Range(position.line, startInt, position.line, endInt),
-						someTrackingId: someTrackingIdCounter++,
-					},
-				] as MyInlineCompletionItem[];
-			}
-		},
-	};
+	const provider: vscode.InlineCompletionItemProvider = new IntellicodeCompletionProvider();
 
 	vscode.languages.registerInlineCompletionItemProvider({ pattern: '**' }, provider);
 
 }
 
-interface MyInlineCompletionItem extends vscode.InlineCompletionItem {
-	someTrackingId: number;
+class IntellicodeCompletionProvider implements vscode.InlineCompletionItemProvider {
+	public provideInlineCompletionItems(document: vscode.TextDocument, position: vscode.Position, context: vscode.InlineCompletionContext, token: vscode.CancellationToken): 
+	vscode.ProviderResult<vscode.InlineCompletionItem[] | vscode.InlineCompletionList> 
+	{
+		console.log('provideInlineCompletionItems triggered');
+
+		const regexp = /\/\/ \[(.+),(.+)\):(.*)/;
+		if (position.line <= 0) {
+			return;
+		}
+
+		let insertText = "";
+
+		const lineBefore = document.lineAt(position.line - 1).text;
+		const matches = lineBefore.match(regexp);
+		if (matches) {
+			const start = matches[1];
+			const startInt = parseInt(start, 10);
+			const end = matches[2];
+			const endInt =
+				end === '*' ? document.lineAt(position.line).text.length : parseInt(end, 10);
+			insertText = matches[3].replace(/\\n/g, '\n');
+
+			if (context.selectedCompletionInfo !== undefined) {
+				insertText = `${context.selectedCompletionInfo.text}.log()`;
+			}
+
+			return [
+				{
+					insertText,
+				},
+			] as vscode.InlineCompletionItem[];
+		}
+	}
+
+	public handleDidShowCompletionItem(item: vscode.InlineCompletionItem): void
+	{
+		console.log("handleDidShowCompletionItem called");
+	}
 }
+
